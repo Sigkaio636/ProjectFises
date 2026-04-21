df26e124276174375dc6e2722485e6e7888a7bc8
EKI <ekiciclofractal636@gmail.com>
2026-04-21 16:39:27 +0200

    general reaction between species with activation energy

--- main.jl
+++ main.jl
@@ -7,12 +7,12 @@ include("src/IdealGas.jl")
 
 # Ejecución de la simulación con parámetros personalizados
 IdealGas.run(
-    N_A         = 160,
-    N_B         = 5,
+    N_A         = 200,
+    N_B         = 10,
     N_C         = 5,
     Lx          = 120.0,
     Ly          = 120.0,
-    T_init      = 2.0,
+    T_init      = 10.0,
     dt          = 0.2,
     n_steps     = 2000,
     p_react     = 0.05,
